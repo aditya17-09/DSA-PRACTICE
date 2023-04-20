1f2ca88c488539f47d98562bdd0961d0a6d48aa9
ADITYA GAUTAM <aditya.kumar2020@vitbhopal.ac.in>
2023-04-20 22:53:10 +0530

    TODAY CONTRIBUTION

---
+++ p3.cpp
@@ -0,0 +1,11 @@
+//DIVISION OF NUMBER
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a and b"<<endl;
+    cin>>a>>b;
+    cout<<"The division of a and b is "<<a/b;
+    return 0;
+}
