a9c1ad0727a33350b4effe3e18607802843115a1
ADITYA GAUTAM <aditya.kumar2020@vitbhopal.ac.in>
2023-04-21 14:29:24 +0530

    UPDATED COMMIT

---
+++ PATTERNS/NUMERIC_INVERTED_HALF_PYRAMID.cpp
@@ -0,0 +1,17 @@
+//NUMERIC INVERTED HALF PYRAMID
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i+1;j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
