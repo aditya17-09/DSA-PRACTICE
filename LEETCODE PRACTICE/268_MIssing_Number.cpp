9f06ad878720ace32b8f85f9b1f7e9889060fbee
ADITYA GAUTAM <aditya.kumar2020@vitbhopal.ac.in>
2023-05-17 21:10:22 +0530

    Missing Number

--- LEETCODE PRACTICE/268_MIssing_Number.cpp
+++ LEETCODE PRACTICE/268_MIssing_Number.cpp
@@ -6,3 +6,21 @@ public int missingNumber(int[] nums) { //xor
     }
     return res;
 }
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int missing = nums.size();
+        for (int i=0; i<nums.size(); i++) {
+            while (nums[i] != i) {
+                if (nums[i] != nums.size())
+                    swap(nums[i], nums[nums[i]]);
+                else {
+                    missing = i;
+                    break;
+                }
+            }
+        }
+        return missing;
+    }
+};
